2bc8b9530699fd8365cd4b6ece5c6e3b78dd7fa7
Akaame <sddkacil.1@gmail.com>
2016-11-07 13:39:18 +0300

    The first working version on the local. Couldn't get it to work on cluster. Jar verification error

--- hadoop_first/src/SumReducer.java
+++ hadoop_first/src/SumReducer.java
@@ -2,29 +2,30 @@
  * Created by sddk on 07.11.2016.
  */
 
-import org.apache.hadoop.io.LongWritablr;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Reducer
+import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.mapreduce.lib.input.FileInputFormat;
-import org.apache.mapreduce.lib.output.FileOutputFormat;
-import org.apache.mapreduce.lib.reduce.LongSumReducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.reduce.LongSumReducer;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import java.io.IOException;
-public class SumReducer<T> extends Reducer<T,LongWritable,T,LongWritable> throws IOException,InterruptedException{
+
+public class SumReducer<T> extends Reducer<T,LongWritable,T,LongWritable> {
 
 private LongWritable count = new LongWritable();
 
-public void reduce(T key,Iterable<LongWritable> values,Context ctx) {
+public void reduce(T key,Iterable<LongWritable> values,Context ctx) throws IOException,InterruptedException {
         long sum = 0;
         for (LongWritable l:values){
-        sum+=l.get();
+            sum+=l.get();
         }
-        result.set(sum);
+        count.set(sum);
         ctx.write(key,count);
         }
-        }
+}
